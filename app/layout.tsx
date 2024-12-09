c3107c164b6175971a629667dcf5a7b71da8f291
masoodalamdev <107204700+masoodalamdev@users.noreply.github.com>
2024-12-09 23:54:56 +0500

    new

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-
+import Navbar from "./components/Navbar";
+import Banner from "./components/Banner";
+import Footer from "./components/Footer";
+import {Josefin_Sans, Lato} from 'next/font/google'
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -12,6 +15,16 @@ const geistMono = localFont({
   variable: "--font-geist-mono",
   weight: "100 900",
 });
+
+const josefinSans = Josefin_Sans({
+  subsets: ['latin'],
+  variable: "--font-josefin-sans"
+})
+const lato = Lato({
+  subsets: ['latin'],
+  variable: "--font-lato",
+  weight: ["100", "900"]
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,9 +39,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${josefinSans.variable} ${lato.variable} antialiased `}
       >
+        <Banner/>
+        <Navbar/>
         {children}
+        <Footer />
       </body>
     </html>
   );
